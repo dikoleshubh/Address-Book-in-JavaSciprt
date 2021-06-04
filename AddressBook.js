bd521b0041b78673add49c500da7069c7488f93c
dikoleshubh <dikole.shubh@gmail.com>
2021-06-04 11:17:59 +0530

    Create Address Book

--- AddressBook.js
+++ AddressBook.js
@@ -1,107 +1,38 @@
-//UC1 Create address book
-console.log("Welcome To Address Book")
-class AddressBook
-{
+class Contact {
+
     firstName;
     lastName;
     address;
     city;
     state;
     zip;
-    phoneNumber;
+    mobileNumber;
     email;
 
-    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email)
-    {
+    constructor (firstName,lastName,address,city,state,zip,mobileNumber,email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
         this.city = city;
         this.state = state;
         this.zip = zip;
-        this.phoneNumber = phoneNumber;
+        this.mobileNumber = mobileNumber;
         this.email = email;
     }
 
-    get firstName()
-    {
-        return this._firstName;
-    }
-    set firstName(firstName)
-    {
-        this._firstName = firstName;
-    }
-
-    get lastName()
-    {
-        return this._lastName;
-    }
-    set lastName(lastName)
-    {
-        this._lastName = this.lastName;
-    }
-
-    get address()
-    {
-        return this._address;
-    }
-    set address(address)
-    {
-        this._address = this.address;
-    }
-
-    get city()
-    {
-        return this._city;
-    }
-    set city(city)
-    {
-        this._city = city;
-    }
-
-    get state()
-    {
-        return this._state;
-    }
-    set state(state)
-    {
-        this._state = state;
-    }
-
-    get zip()
-    {
-        return this._zip;
-    }
-    set zip(zip)
-    {
-        this._zip = zip;
-    }
-
-    get phoneNumber()
-    {
-        return this._phoneNumber;
-    }
-    set phoneNumber(phoneNumber)
-    {
-        this._phoneNumber = this.phoneNumber;
-    }
-
-    get email()
-    {
-        return this._email;
-    }
-    set email(email)
-    {
-        this._email = this.email;
-    }
-
-    toString()
-    {
-        return "FirstName : " + this.firstName + "\nLastName : " + this.lastName + "\nAddress :" + this.address +
-        "\nCity : " + this.city + "\nState : " + this.state + "\nZip : " + this.zip + "\nPhoneNumber : " + this.phoneNumber +
-        "\nemail : " + this.email;
+    toString() {
+        return "\nFirst Name    : " + this.firstName +
+        "\nLast Name     : " + this.lastName +
+        "\nAddress       : " + this.address +
+        "\nCity          : " + this.city +
+        "\nState         : " + this.state +
+        "\nZip           : " + this.zip +
+        "\nMobile Number : " + this.mobileNumber +
+        "\nEmail         : " + this.email;
     }
 }
+console.log("<<<<<Welcome to AddressBook Application>>>>>");
 
-let addressBook = new AddressBook("Venu", "Champa", "Pune", "MH", "MH", 434139, 99244255, "labda123@gmail.com");
-console.log(addressBook.toString());
+//UC1 - Ability to Create Contact in Address Book
+let Profile = new Contact("Venu","Champa","COEP","Pune","Maharastr",444505,865343543,"venu@gmail.com");
+console.log(Profile.toString());
